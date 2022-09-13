50bdde114c091e1604591b3cafba0de486e19049
Kaio Rodrigues <chrno.92@gmail.com>
2022-09-12 23:59:49 -0300

    feat: add info sobre operadores numericos + atualizacao das infos sobre variaveis

---
+++ dados/Number/example.js
@@ -0,0 +1,51 @@
+//toExponential()
+
+// let numberExponential = 123.4;
+// console.log(numberExponential.toExponential(10));
+
+//toFixed()
+
+// let numberFixed = 123.4;
+// console.log(numberFixed.toFixed(2));
+
+//toPrecision()
+
+// let numberPrecision = 123.4;
+// console.log(numberPrecision.toPrecision(10));
+
+//Operadores aritméticos
+
+// console.log(2 + 2);
+// console.log(8 - 3);
+// console.log(4 * 5);
+// console.log(9 / 3);
+// console.log(7 % 2);
+
+//Operadores de atribuição
+
+// let numberAtribuicao = 10;
+
+// console.log(numberAtribuicao += 2);
+// console.log(numberAtribuicao -= 2);
+// console.log(numberAtribuicao *= 2);
+// console.log(numberAtribuicao /= 2);
+// console.log(numberAtribuicao %= 2);
+
+//Operadores de incremento e decremento
+
+// let numberIncDec = 20;
+
+// console.log(numberIncDec--); // pos incremento
+// console.log(numberIncDec++); // pos incremento
+// console.log(--numberIncDec); // pre incremento
+// console.log(++numberIncDec); // pre incremento
+
+// Operadores Binários
+
+// console.log(4 | 3);
+// console.log(3 & 1);
+// console.log(5 ^ 2);
+// console.log(~2);
+// console.log(4 << 2);
+// console.log(128 >> 1);
+// console.log(-2 >>> 1);
---
+++ variaveis/example.js
@@ -0,0 +1,33 @@
+// var pi = 3.141592;
+// console.log(pi);
+// var pi = 3;
+// console.log(pi);
+
+// if (true) {
+//     let pi = 3.141592;
+// };
+
+// console.log(pi);
+
+// const pi = 3.141592;
+// pi = 3;
+// console.log(pi);
+
+// Exemplo de identificadores válidos:
+
+// let nome;
+// let Nome;
+// let _nome;
+// let $nome;
+// let nome123;
+// let Nome123;
+// let _nome123;
+// let $nome123;
+
+// Exemplo de identificadores que não
+// são válidos:
+
+// let 123nome;
+// let @nome;
+// let ''nome;
+// let nome-sobrenome;
