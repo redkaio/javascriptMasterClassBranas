7e1c99fedc8267612c97b15b5edfd6af7f693f95
Kaio Rodrigues <chrno.92@gmail.com>
2022-09-18 09:45:41 -0300

    feat: iniciar modulo de strings com explicacao de como seu tipo funciona

---
+++ dados/String/example.js
@@ -0,0 +1,48 @@
+// let string1 = 'JavaScript';
+// let string2 = "JavaScript";
+// let string3 = `JavaScript`;
+
+// Criando através de uma função construtora
+
+// new String('JavaScript');
+// new String("JavaScript");
+// new String(`JavaScript`);
+
+// Comparação de tempo de criação de Strings utilizando uma função construtora
+
+// let counter = 0;
+
+// console.time("performance");
+
+// while(counter < 100000) {
+//     "Javascript";
+//     counter++;
+// };
+
+// console.timeEnd("performance");
+
+// let counterConstructor = 0;
+// console.time("performance");
+
+// while(counterConstructor < 100000) {
+//     new String("Javascript");
+//     counterConstructor++;
+// };
+
+// console.timeEnd("performance");
+
+
+// Exemplos de como escapar caracteres especiais
+
+// console.log("Teste usando \"aspas duplas\"");
+// console.log('Teste usando aspa simples\'');
+// console.log("Teste para imprimir contra barra: \\n");
+
+// Exemplos de caracteres de controle
+
+// let daysOfWeek = "0 - Sun\n1 - Mon \n2 - Tue\n3 - Wed\n4 - Thu\n5 - Fri\n6 - Sat"
+// console.log(daysOfWeek);
+
+// Escrevendo o mesmo texto de dias da semana utilizando o padrão Unicode
+
+// console.log("\u0030 \u002d \u0053\u0075\u006e\u000A");
